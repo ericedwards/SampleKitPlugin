6db407445f7fa1f1f944ac3647d9bb2af53261eb
Eric Edwards <ericedwards37@gmail.com>
2021-02-26 22:17:19 -0500

    Added more items.

--- src/main/java/io/github/ericedwards/SampleKitPlugin/KitCommand.java
+++ src/main/java/io/github/ericedwards/SampleKitPlugin/KitCommand.java
@@ -17,9 +17,15 @@ public class KitCommand implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
         if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
+            logger.info("Giving " + player.getDisplayName() + " a kit");
             ItemStack sword = new ItemStack(Material.DIAMOND_SWORD);
-            logger.info("Giving " + player.getDisplayName() + " a diamond sword");
+            ItemStack shield = new ItemStack(Material.SHIELD);
+            ItemStack chestPlate = new ItemStack(Material.DIAMOND_CHESTPLATE);
+            ItemStack helmet = new ItemStack(Material.DIAMOND_HELMET);
             player.getInventory().addItem(sword);
+            player.getInventory().addItem(shield);
+            player.getInventory().addItem(chestPlate);
+            player.getInventory().addItem(helmet);
         }
         return true;
     }
